11c55bc94dad2c050cf67573d7a105b143d0a875
Amul-Thantharate <amulthantharate@gmail.com>
2024-04-02 18:40:31 +0530

    Add Amplify dependencies and configuration files

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "quizapp24baa70a": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
